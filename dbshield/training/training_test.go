8da289cfc47e0af08dea9446b24ce4d7d8b35a05
qiwihui <qwh005007@gmail.com>
2017-10-17 14:56:36 +0800

    test with CheckQuery and AddToTrainingSet

--- dbshield/training/training_test.go
+++ dbshield/training/training_test.go
@@ -1,42 +1,74 @@
 package training_test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"log"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/astaxie/beego/orm"
 	"github.com/boltdb/bolt"
-	"github.com/nim4/DBShield/dbshield/config"
-	"github.com/nim4/DBShield/dbshield/sql"
-	"github.com/nim4/DBShield/dbshield/training"
+	"github.com/qiwihui/DBShield/dbshield/config"
+	"github.com/qiwihui/DBShield/dbshield/db"
+	"github.com/qiwihui/DBShield/dbshield/sql"
+	"github.com/qiwihui/DBShield/dbshield/training"
 )
+
+var c0 sql.QueryContext = sql.QueryContext{
+	Query:    []byte("select * from test;"),
+	Database: []byte("test"),
+	User:     []byte("test"),
+	Client:   []byte("127.0.0.1"),
+	Time:     time.Now(),
+}
+
+var mysql = db.MySQL{}
+var boltDB = db.BoltDB{}
 
 func TestMain(m *testing.M) {
 	log.SetOutput(ioutil.Discard) // Avoid log outputs
+
+	var dbDsn string = "root:password@tcp(localhost:3306)/dbshield?charset=utf8"
+	// var mysql = db.MySQL{}
+	// config.Config.LocalDB = &mysql
+	err := mysql.InitialDB(dbDsn, 0, 0)
+	if err != nil {
+		fmt.Println("Got error", err)
+	}
+	pattern := sql.Pattern(c0.Query)
+	mysql.AddPattern(pattern, c0)
+
 	tmpfile, err := ioutil.TempFile("", "testdb")
 	if err != nil {
 		panic(err)
 	}
 	defer tmpfile.Close()
 	path := tmpfile.Name()
-	training.DBCon, err = bolt.Open(path, 0600, nil)
+	// boltDB := new(db.BoltDB)
+	// config.Config.LocalDB = &mysql
+	err = boltDB.InitialDB(path, 0, 0)
 	if err != nil {
-		panic(err)
+		fmt.Println("Got error", err)
 	}
-	training.DBCon.Update(func(tx *bolt.Tx) error {
-		tx.CreateBucket([]byte("pattern"))
-		tx.CreateBucket([]byte("abnormal"))
-		tx.CreateBucket([]byte("state"))
-		return nil
-	})
-	m.Run()
+
+	os.Exit(m.Run())
 }
 
-func TestAddToTrainingSet(t *testing.T) {
+func switchDB(usingDB string) {
+	if usingDB == "mysql" {
+		config.Config.LocalDB = &mysql
+	} else {
+		config.Config.LocalDB = &boltDB
+	}
+}
+
+func TestAddToTrainingSetWithBoltDB(t *testing.T) {
+	switchDB("boltdb")
 	var err error
 	c := sql.QueryContext{
-		Query:    []byte("select * from test;"),
+		Query:    []byte("select * from ExpectedTest;"),
 		Database: []byte("test"),
 		User:     []byte("test"),
 		Client:   []byte("127.0.0.1"),
@@ -47,17 +79,18 @@ func TestAddToTrainingSet(t *testing.T) {
 		t.Error("Not Expected error", err)
 	}
 
-	tmpCon := training.DBCon
+	// Add without buckets
+	tmpDB := db.DBCon
 	defer func() {
-		training.DBCon = tmpCon
+		db.DBCon = tmpDB
 	}()
-	tmpfile, err := ioutil.TempFile("", "testdb")
+	tmpfile, err := ioutil.TempFile("", "testdbWithoutBuckets")
 	if err != nil {
 		panic(err)
 	}
 	defer tmpfile.Close()
 	path := tmpfile.Name()
-	training.DBCon, err = bolt.Open(path, 0600, nil)
+	db.DBCon, err = bolt.Open(path, 0600, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -67,18 +100,19 @@ func TestAddToTrainingSet(t *testing.T) {
 	}
 }
 
-func TestCheckQuery(t *testing.T) {
+func TestCheckQueryWithBotDB(t *testing.T) {
+	switchDB("boltdb")
 	config.Config.CheckUser = true
 	config.Config.CheckSource = true
 	c1 := sql.QueryContext{
-		Query:    []byte("select * from test;"),
+		Query:    []byte("select * from c1test;"),
 		Database: []byte("test"),
 		User:     []byte("test"),
 		Client:   []byte("127.0.0.1"),
 		Time:     time.Now(),
 	}
 	c2 := sql.QueryContext{
-		Query:    []byte("select * from user;"),
+		Query:    []byte("select * from c2user;"),
 		Database: []byte("test"),
 		User:     []byte("test"),
 		Client:   []byte("127.0.0.1"),
@@ -92,9 +126,9 @@ func TestCheckQuery(t *testing.T) {
 		t.Error("Expected true")
 	}
 
-	tmpCon := training.DBCon
+	tmpDB := db.DBCon
 	defer func() {
-		training.DBCon = tmpCon
+		db.DBCon = tmpDB
 	}()
 	tmpfile, err := ioutil.TempFile("", "testdb")
 	if err != nil {
@@ -102,11 +136,11 @@ func TestCheckQuery(t *testing.T) {
 	}
 	defer tmpfile.Close()
 	path := tmpfile.Name()
-	training.DBCon, err = bolt.Open(path, 0600, nil)
+	db.DBCon, err = bolt.Open(path, 0600, nil)
 	if err != nil {
 		panic(err)
 	}
-	training.DBCon.Update(func(tx *bolt.Tx) error {
+	db.DBCon.Update(func(tx *bolt.Tx) error {
 		tx.CreateBucket([]byte("pattern"))
 		return err
 	})
@@ -118,17 +152,39 @@ func TestCheckQuery(t *testing.T) {
 	training.CheckQuery(c1)
 }
 
-func BenchmarkAddToTrainingSet(b *testing.B) {
+func BenchmarkAddToTrainingSetWithMysql(b *testing.B) {
+	switchDB("mysql")
+	o := orm.NewOrm()
 	b.ReportAllocs()
 	b.ResetTimer()
-	c := sql.QueryContext{
-		Query:    []byte("select * from test;"),
-		Database: []byte("test"),
-		User:     []byte("test"),
-		Client:   []byte("127.0.0.1"),
-		Time:     time.Now(),
-	}
 	for i := 0; i < b.N; i++ {
-		training.AddToTrainingSet(c)
+		training.AddToTrainingSet(c0)
+		b.StopTimer()
+		// clean db to add instead of just checking then passed
+		_ = o.Raw("delete from pattern")
+		b.StartTimer()
+	}
+}
+
+func BenchmarkAddToTrainingSetWithBoltDB(b *testing.B) {
+	switchDB("boltdb")
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		training.AddToTrainingSet(c0)
+		b.StopTimer()
+		// clean db to add instead of just checking then passed
+		db.DBCon.Update(func(tx *bolt.Tx) error {
+			err := tx.DeleteBucket([]byte("pattern"))
+			if err != nil {
+				return fmt.Errorf("delete bucket: %s", err)
+			}
+			_, err = tx.CreateBucket([]byte("pattern"))
+			if err != nil {
+				return fmt.Errorf("create bucket: %s", err)
+			}
+			return nil
+		})
+		b.StartTimer()
 	}
 }
